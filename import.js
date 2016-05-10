d014d1bb9b30b432cfd6ee48f45b172fe1743f12
Pedro Dias <petermdias@gmail.com>
2016-05-10 04:02:18 +0100

    importer customizable log path

--- import.js
+++ import.js
@@ -1,6 +1,11 @@
 var SignalImporter = require('./lib/SignalImporter');
 
-var s = new SignalImporter();
+if(process.argv.length !== 3) {
+  console.log('Example: "node import.js /home/probe/logs/"');
+  process.exit(1);
+}
+
+var s = new SignalImporter(process.argv[2]);
 s.start(function() {
   process.exit(0);
 });
